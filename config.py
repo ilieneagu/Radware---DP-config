584fe151179fcda12ed69718aa964a2bd36e350c
ilie neagu <ilie.neagu@gmail.com>
2023-11-23 11:32:45 -0500

    add requirement.txt

--- config.py
+++
@@ -1,5 +0,0 @@
-# Sample config.py file
-VISION_IP = "x.x.x.x"          # APSolute Vision IP or FQDN
-VISION_USER = "my_user"        # APSolute Vision username
-VISION_PASS = "my_password"    # APSolute Vision password
-DefensePro_MGMT_IP = ["172.16.1.1", "10.1.1.1"]  # DefensePro IP list in this format
