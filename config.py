043a1323915150b0c7f1f34ec54a607e52480b73
ilie neagu <ilie.neagu@gmail.com>
2023-12-22 08:30:56 -0500

    cfg

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-VISION_IP = "x.x.x.x"          # APSolute Vision IP or FQDN
-VISION_USER = "my_user"        # APSolute Vision username
-VISION_PASS = "my_password"    # APSolute Vision password
-DefensePro_MGMT_IP = ["172.16.1.1", "10.1.1.1"]  # DefensePro IP list in this format
+VISION_IP = "visionq.radwarelabs.ca"          # APSolute Vision IP or FQDN
+VISION_USER = "radware"        # APSolute Vision username
+VISION_PASS = "RadSE2023!"    # APSolute Vision password
+DefensePro_MGMT_IP = ["172.27.200.165"]  # DefensePro IP list in this format
